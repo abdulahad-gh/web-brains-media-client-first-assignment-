3b19e3b91e34dd31ca9e1a4871b7481258c47265
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-08-26 20:14:13 +0600

    create file structure and declare constants

---
+++ src/services/constants/userConstants.js
@@ -0,0 +1,5 @@
+export const CREATE_SUBJECT = 'CREATE_SUBJECT';
+export const ADD_TOPIC = 'ADD_TOPIC';
+export const ADD_NOTE = 'ADD_NOTE';
+export const EDIT = 'EDIT';
+export const DELETE = 'DELETE';
