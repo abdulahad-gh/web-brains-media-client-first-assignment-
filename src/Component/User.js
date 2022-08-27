92a8f9d061b518c476c0de9e386e762ead312c60
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-08-28 01:39:03 +0600

    created table, and show booklist on ui

--- src/Component/User.js
+++ src/Component/User.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
-import { ErrorText, Form, Label, Title } from '../StyledComponents/WebBrainsMedia.Styled';
+import { DeleteButton, EditButton, ErrorText, Form, Label, Table, Td, Th, Title, Tr } from '../StyledComponents/WebBrainsMedia.Styled';
 
 const User = () => {
     const [userActions, setUserActions] = useState({
         isLoading: false,
-        subjects: [],
-        topics: [],
-        notes: [],
+        subjects: [{ id: 0, subjectName: '', topic: '', note: '', }],
+
         error: null
     })
+    const [updateSubject, setUpdateSubject] = useState({
+        updateSubjectName: '',
+        updatedTopic: '',
+        updatedNote: ''
+    })
+    const [edit, setEdit] = useState(false)
+    const handleUpdate = (id) => {
+        console.log(id)
+    }
 
     const handleCreateBook = (e) => {
         e.preventDefault()
@@ -20,18 +28,19 @@ const User = () => {
             })
             return
         }
-        const exists = userActions.subjects.find(sub => sub === subject)
+        const exists = userActions.subjects.find(sub => sub.subjectName === subject)
+
         if (exists) {
             setUserActions({
                 ...userActions,
-                error: `your ${exists} book is already exists!`
+                error: `your ${exists.subjectName} book is already exists!`
             })
             return
         }
 
         setUserActions({
             ...userActions,
-            subjects: [...userActions.subjects, subject],
+            subjects: [...userActions.subjects, { id: userActions.subjects.length + 1, subjectName: subject, topic: 'topic', note: 'note' }],
             error: null
 
 
@@ -39,7 +48,7 @@ const User = () => {
 
 
     }
-    // console.log(userActions.subjects)
+    console.log(userActions.subjects)
     return (
         <section>
             <Title>Student actions</Title>
@@ -54,6 +63,44 @@ const User = () => {
 
                 </div>
             </Form>
+            <Table>
+                <thead>
+                    <Tr>
+                        <Th>
+                            Subjects
+                        </Th>
+                        <Th>
+                            {edit ? 'Actions' : 'Action'}
+                        </Th>
+                    </Tr>
+                </thead>
+                <tbody>
+                    {
+                        userActions.subjects.map((subject, index) => {
+                            if (subject.subjectName) {
+                                return <Tr key={index}>
+                                    <Td>
+                                        <div style={{ paddingLeft: '20px' }}>
+                                            <li >
+                                                <input style={{ border: 'none', fontWeight: 'bold', fontSize: '16px', }} onChange={(e) => setUpdateSubject({ ...updateSubject, updateSubjectName: e.target.text })} placeholder={subject.subjectName} />
+                                            </li>
+                                            <input name='topic' onChange={(e) => setUpdateSubject({ ...updateSubject, updatedTopic: e.target.value })} style={{ display: 'block', marginTop: '10px', width: '20%' }} type="text" placeholder='topic' />
+                                            <textarea name='note' onChange={(e) => setUpdateSubject({ ...updateSubject, updatedNote: e.target.value })} placeholder='note' id="" cols="10" style={{ display: 'block', width: '30%', resize: 'none', marginTop: '2px' }}></textarea>
+                                            <button onClick={() => handleUpdate(subject.id)} style={{ display: 'block', width: '20%', margin: 'auto' }}>Update All</button>
+                                        </div>
+                                    </Td>
+                                    <Td style={{ textAlign: 'center' }}>
+                                        {edit && <EditButton>Edit</EditButton>}
+                                        <DeleteButton>Delete</DeleteButton>
+                                    </Td>
+                                </Tr>
+                            }
+
+
+                        })
+                    }
+                </tbody>
+            </Table>
 
 
         </section>
--- src/StyledComponents/WebBrainsMedia.Styled.js
+++ src/StyledComponents/WebBrainsMedia.Styled.js
@@ -2,10 +2,12 @@ import styled from 'styled-components'
 
 export const Form = styled.form`
 background:rgba(40,20,60,.4);
-padding:2rem;
+padding:1rem;
 color:white;
-width: 50%;
-margin:auto
+width: 40%;
+margin:70px auto 40px;
+border-radius:6px;
+text-align:center
 `
 export const Title = styled.h1`
 text-align:center;
@@ -15,4 +17,42 @@ display:block;
 `
 export const ErrorText = styled.p`
 color:red
+`
+export const Table = styled.table`
+width: 60%;
+margin:auto;
+border-collapse:collapse;
+`
+export const Tr = styled.tr`
+border-collapse:collapse;
+border:1px solid;
+`
+export const Th = styled.th`
+border-collapse:collapse;
+border:1px solid;
+`
+export const Td = styled.td`
+border-collapse:collapse;
+border:1px solid;
+`
+export const EditButton = styled.button`
+background:palevioletred;
+color:white;
+font-size: 1em;
+margin: .6em;
+padding: 0.25em 1em;
+border: 2px solid palevioletred;
+border-radius: 3px;
+cursor:pointer;
+`
+export const DeleteButton = styled.button`
+background:orange;
+color:white;
+font-size: 1em;
+margin: .6em;
+padding: 0.25em 1em;
+border: 2px solid orange;
+border-radius: 3px;
+cursor:pointer;
+
 `
