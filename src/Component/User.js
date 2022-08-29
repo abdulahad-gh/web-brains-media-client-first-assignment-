d489f4fba86641465375fb7c25b7335e71609905
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-08-29 13:45:25 +0600

    added delete subject feature

--- src/Component/User.js
+++ src/Component/User.js
@@ -4,7 +4,7 @@ import { DeleteButton, EditButton, ErrorText, Form, Label, Table, Td, Th, Title,
 const User = () => {
     const [userActions, setUserActions] = useState({
         isLoading: false,
-        subjects: [{ id: 0, subjectName: '', topic: '', note: '', }],
+        subjects: [{ id: 1, subjectName: '', topic: '', note: '', }],
 
         error: null
     })
@@ -15,7 +15,21 @@ const User = () => {
     })
     const [edit, setEdit] = useState(false)
     const handleUpdate = (id) => {
-        console.log(id)
+        // userActions.subjects.forEach(sub => {
+        //     if (sub.id === id) {
+        //         const { updateSubjectName, updatedTopic, updatedNote } = updateSubject;
+        //         setUserActions({...userActions,subjects:[...userActions.subjects].forEach()})
+
+        //     }
+
+        // })
+
+    }
+
+    //handle delete
+    const handleDelete = (id) => {
+        const newSubjectContainer = userActions.subjects.filter(sub => sub.id !== id)
+        setUserActions({ ...userActions, subjects: newSubjectContainer })
     }
 
     const handleCreateBook = (e) => {
@@ -48,7 +62,6 @@ const User = () => {
 
 
     }
-    console.log(userActions.subjects)
     return (
         <section>
             <Title>Student actions</Title>
@@ -56,8 +69,8 @@ const User = () => {
             <Form onSubmit={handleCreateBook}>
 
                 <div>
-                    <Label htmlFor="createBook">Create book--</Label>
-                    <input type="text" name='book' />
+                    <Label htmlFor="createBook">--Create book--</Label>
+                    <input type="text" name='book' onFocus={() => setUserActions({ ...userActions, error: '' })} />
                     <button>Add book</button>
                     <ErrorText>{userActions.error}</ErrorText>
 
@@ -82,16 +95,16 @@ const User = () => {
                                     <Td>
                                         <div style={{ paddingLeft: '20px' }}>
                                             <li >
-                                                <input style={{ border: 'none', fontWeight: 'bold', fontSize: '16px', }} onChange={(e) => setUpdateSubject({ ...updateSubject, updateSubjectName: e.target.text })} placeholder={subject.subjectName} />
+                                                <input name='subjectName' style={{ border: 'none', fontWeight: 'bold', fontSize: '16px', }} onChange={(e) => setUpdateSubject({ ...updateSubject, updateSubjectName: e.target.value })} placeholder={subject.subjectName} />
                                             </li>
                                             <input name='topic' onChange={(e) => setUpdateSubject({ ...updateSubject, updatedTopic: e.target.value })} style={{ display: 'block', marginTop: '10px', width: '20%' }} type="text" placeholder='topic' />
                                             <textarea name='note' onChange={(e) => setUpdateSubject({ ...updateSubject, updatedNote: e.target.value })} placeholder='note' id="" cols="10" style={{ display: 'block', width: '30%', resize: 'none', marginTop: '2px' }}></textarea>
-                                            <button onClick={() => handleUpdate(subject.id)} style={{ display: 'block', width: '20%', margin: 'auto' }}>Update All</button>
+                                            <button onClick={(e) => handleUpdate(subject.id)} style={{ display: 'block', width: '20%', margin: 'auto' }}>Update All</button>
                                         </div>
                                     </Td>
                                     <Td style={{ textAlign: 'center' }}>
                                         {edit && <EditButton>Edit</EditButton>}
-                                        <DeleteButton>Delete</DeleteButton>
+                                        <DeleteButton onClick={() => handleDelete(subject.id)}>Delete</DeleteButton>
                                     </Td>
                                 </Tr>
                             }
