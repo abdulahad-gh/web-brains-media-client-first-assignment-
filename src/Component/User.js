2f390a5816b2f243c9653e451e033a4edd3aaf33
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-09-03 22:40:22 +0600

    chanage title and completed web_brains_media assignment, but database not itregade, it is not big dill for me.

--- src/Component/User.js
+++ src/Component/User.js
@@ -8,21 +8,24 @@ const User = () => {
 
         error: null
     })
+    // console.log(userActions.subjects[0])
     const [updateSubject, setUpdateSubject] = useState({
         updateSubjectName: '',
         updatedTopic: '',
         updatedNote: ''
     })
+    console.log(updateSubject.updatedTopic);
+
     const [edit, setEdit] = useState(false)
-    const handleUpdate = (id) => {
-        // userActions.subjects.forEach(sub => {
-        //     if (sub.id === id) {
-        //         const { updateSubjectName, updatedTopic, updatedNote } = updateSubject;
-        //         setUserActions({...userActions,subjects:[...userActions.subjects].forEach()})
+    const handleUpdate = (id, e) => {
+        // const { updateSubjectName, updatedTopic, updatedNote } = updateSubject;
+        e.preventDefault()
+        const subject = e.target.subjectName.value;
+        userActions.subjects[1] = subject
+        userActions.subjects[2] = updateSubject.updatedTopic
+        console.log('line 24', userActions.subjects)
+        setEdit(true)
 
-        //     }
-
-        // })
 
     }
 
@@ -56,7 +59,6 @@ const User = () => {
             ...userActions,
             subjects: [...userActions.subjects, { id: userActions.subjects.length + 1, subjectName: subject, topic: 'topic', note: 'note' }],
             error: null
-
 
         })
 
@@ -93,14 +95,22 @@ const User = () => {
                             if (subject.subjectName) {
                                 return <Tr key={index}>
                                     <Td>
-                                        <div style={{ paddingLeft: '20px' }}>
-                                            <li >
-                                                <input name='subjectName' style={{ border: 'none', fontWeight: 'bold', fontSize: '16px', }} onChange={(e) => setUpdateSubject({ ...updateSubject, updateSubjectName: e.target.value })} placeholder={subject.subjectName} />
-                                            </li>
-                                            <input name='topic' onChange={(e) => setUpdateSubject({ ...updateSubject, updatedTopic: e.target.value })} style={{ display: 'block', marginTop: '10px', width: '20%' }} type="text" placeholder='topic' />
-                                            <textarea name='note' onChange={(e) => setUpdateSubject({ ...updateSubject, updatedNote: e.target.value })} placeholder='note' id="" cols="10" style={{ display: 'block', width: '30%', resize: 'none', marginTop: '2px' }}></textarea>
-                                            <button onClick={(e) => handleUpdate(subject.id)} style={{ display: 'block', width: '20%', margin: 'auto' }}>Update All</button>
-                                        </div>
+                                        <form onSubmit={(e) => handleUpdate(subject.id, e)}>
+
+                                            <div style={{ paddingLeft: '20px' }}>
+                                                <li >
+                                                    <input placeholder={subject.subjectName} name='subjectName' style={{ border: 'none', fontWeight: 'bold', fontSize: '16px', }} />
+                                                </li>
+
+                                                <p>
+                                                    <input name='topic' onChange={(e) => setUpdateSubject({ ...updateSubject, updatedTopic: e.target.value })} style={{ marginTop: '10px', width: '20%' }} type="text" placeholder='topic' /> <span>{subject.topic}</span>
+                                                </p>
+
+                                                <textarea name='note' onChange={(e) => setUpdateSubject({ ...updateSubject, updatedNote: e.target.value })} placeholder='note' id="" cols="10" style={{ display: 'block', width: '30%', resize: 'none', marginTop: '2px' }}></textarea>
+
+                                                <button style={{ display: 'block', width: '20%', margin: 'auto' }}>Update All</button>
+                                            </div>
+                                        </form>
                                     </Td>
                                     <Td style={{ textAlign: 'center' }}>
                                         {edit && <EditButton>Edit</EditButton>}
